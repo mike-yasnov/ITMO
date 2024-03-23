b57af4129cc81f82c6657583f54ff54f36ee4cd5
Yasnov Mikhail <mike_yasnov@mail.ru>
2024-03-23 11:14:15 +0300

    Create XmlFileWriter.java

---
+++ Программирование/Lab5/src/utils/XmlFileWriter.java
@@ -0,0 +1,22 @@
+package utils;
+
+import org.w3c.dom.Document;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+
+public class XmlFileWriter {
+    public void writeXmlFile(Document document, String filePath) {
+        try {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new File(filePath));
+            transformer.transform(source, result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
